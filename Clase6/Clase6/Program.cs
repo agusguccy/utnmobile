14f320343d8e0b759d5f8572771f68f8497979f5
agus.sr@hotmail.com <agus.sr@hotmail.com>
2019-09-30 21:57:55 -0300

    actualizacion

--- Clase6/Clase6/Program.cs
+++ Clase6/Clase6/Program.cs
@@ -10,26 +10,37 @@ namespace clase_6
             //Sortee 10 numeros y luego muestre en pantalla el promedio de los mismos
 
 
-            int i = 0;
-            float num, suma = 0;
-            Random objetoRandom = new Random();
+            /* int i = 0;
+             float num, suma = 0;
+             Random objetoRandom = new Random();
 
-            while (i < 10)
-            {
-                
-                num = objetoRandom.Next(1000);
+             while (i < 10)
+             {
 
-                suma += num;
-                i++;
+                 num = objetoRandom.Next(1000);
 
-                Console.WriteLine(i);
+                 suma += num;
+                 i++;
 
-            }
+                 Console.WriteLine(i);
 
-            Console.WriteLine("El promedio de los 10 números es: " + suma / 10);
+             }
+
+             Console.WriteLine("El promedio de los 10 números es: " + suma / 10);
+             */
 
             //Ingrese un número y calcule el factorial del mismo. El factorial de un número (Ej: 5), es:
+            int numero, resultado = 1;
 
+            Console.WriteLine("Ingrese un número a factorizar");
+            numero = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= numero; i++)
+            {
+                resultado = resultado * i;
+            }
+
+            Console.WriteLine("El factorial de " + numero + " es: " + resultado);
 
         }
     }

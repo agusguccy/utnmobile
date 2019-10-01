e9c62a7b2c1d04ee4cb0f32c65418a3fdfe037fc
agus.sr@hotmail.com <agus.sr@hotmail.com>
2019-09-30 21:52:22 -0300

    actualizacion

---
+++ Clase6/Clase6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace clase_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Resolver con FOR y WHILE
+            //Sortee 10 numeros y luego muestre en pantalla el promedio de los mismos
+
+
+            int i = 0;
+            float num, suma = 0;
+            Random objetoRandom = new Random();
+
+            while (i < 10)
+            {
+                
+                num = objetoRandom.Next(1000);
+
+                suma += num;
+                i++;
+
+                Console.WriteLine(i);
+
+            }
+
+            Console.WriteLine("El promedio de los 10 números es: " + suma / 10);
+
+            //Ingrese un número y calcule el factorial del mismo. El factorial de un número (Ej: 5), es:
+
+
+        }
+    }
+}
